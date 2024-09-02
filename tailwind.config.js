6abfc44f3a1f9ce882804ffdb71b2c3f93da4611
arishavellekarl.villanueva <ArishavelleKarl.Villanueva@blackfort.ph>
2024-09-02 14:01:23 +0800

    add flowbite soon implement carousel

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./*.{html,js}"],
+  content: ["./*.{html,js}", "./node_modules/flowbite/**/*.js"],
   theme: {
     screens: {
       sm: "480px",
@@ -21,5 +21,5 @@ module.exports = {
       },
     },
   },
-  plugins: [],
+  plugins: [require("flowbite/plugin")],
 };
